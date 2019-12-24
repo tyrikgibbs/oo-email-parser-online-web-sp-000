ae1360d9b46fa32af58a67ef92599277d0cf7064
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-24 01:57:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -25,9 +25,9 @@ class EmailAddressParser
 end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParser.new(emails1)
 parser1.parse
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParser.new(emails2)
 parser2.parse
